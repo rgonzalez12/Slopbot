10a907307f588a8579a969116405ed723adf189a
Ray <raymondalexander.gonzalez@gmail.com>
2025-01-03 18:03:26 -0500

    updated image generation logic and imageurl logic to properly use new syntax

--- index.js
+++ index.js
@@ -36,13 +36,13 @@ client.on('messageCreate', async (message) => {
 
     try {
       // Generate the image via OpenAI
-      const response = await openai.createImage({
+      const response = await openai.images.generate({
         prompt: prompt,
         n: 1,
         size: '512x512',
       });
 
-      const imageUrl = response.data.data[0].url;
+      const imageUrl = response[0].url;
 
       // Send the image URL in Discord
       await message.channel.send(`**Prompt:** ${prompt}\n${imageUrl}`);
